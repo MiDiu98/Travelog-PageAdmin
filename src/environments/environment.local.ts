2404b3559cf85eacd42f2d2bd9467178e67f2079
Le Cam <thantoandoc83@gmail.com>
2020-04-26 22:28:39 +0700

    fix bug

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,6 +2,9 @@ export const environment = {
   production: true,
   backendIP: 'http://127.0.0.1',
   backendPort: '8080',
-  backendPath: 'https://glacial-beach-08827.herokuapp.com'
+  backendPath: 'https://murmuring-brushlands-11698.herokuapp.com'
 };
+
+
+// backendPath: 'https://glacial-beach-08827.herokuapp.com'
 
