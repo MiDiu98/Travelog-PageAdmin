d2ed5c86f1e438048631eb9c87f5fdd386e4819d
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-04-26 22:38:49 +0700

    Admin Login

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  backendIP: 'http://127.0.0.1',
-  backendPort: '8080',
-  backendPath: 'https://glacial-beach-08827.herokuapp.com'
+  // backendIP: 'http://127.0.0.1',
+  // backendPort: '8080',
+  backendPath: 'https://murmuring-brushlands-11698.herokuapp.com'
 };
 
